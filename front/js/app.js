e8b4d46ce51e8ba9d76a27a96f3955ddc760f04a
Thiago Santana <thiago.santana@gmail.com>
2018-03-06 23:27:52 -0300

    dev

--- front/js/app.js
+++ front/js/app.js
@@ -1,18 +1,59 @@
 (function() {
 	console.log("Module ok");
-	ajax.onload = function() {
-		if (ajax.status == STATUS_OK) {
-			let result = document.getElementById("result");
+	let callback = data1 => {
+		let permutacoes = JSON.parse(data1.currentTarget.response);
+		console.log(permutacoes);
+		for (permutacao in permutacoes.permutacoes) {
+			criaElemento(permutacoes.permutacoes[permutacao], SUCESSO_STATUS);
 		}
 	};
 	let btn_permuta = document.getElementById("permut");
 	let txt_number = document.getElementById("number");
+	let repeat = document.getElementById("repeat-option");
 	txt_number.onkeypress = event => {
 		//apenas numeros
 		return event.charCode >= 48 && event.charCode <= 57;
 	};
 	btn_permuta.onclick = () => {
 		let currentValue = txt_number.value;
-		ajax.send("number=" + currentValue);
+		if (!currentValue) {
+			criaElemento("Vc precisa digitar um número!", ERRO_STATUS);
+			return false;
+		} else {
+			removeStatus();
+			if (repeat.checked) {
+				makeRequest(
+					"number=" + currentValue + "&repeat=true",
+					callback
+				);
+			} else {
+				makeRequest(
+					"number=" + currentValue + "&repeat=false",
+					callback
+				);
+			}
+		}
 	};
+	function criaElemento(strPermutacao, status) {
+		let newElement = document.createElement("div");
+		newElement.classList.add(status);
+		newElement.innerHTML = strPermutacao;
+		result.appendChild(newElement);
+	}
+	function removeStatus() {
+		let result = document.getElementById("result");
+		while (result.firstChild) {
+			result.removeChild(result.firstChild);
+		}
+	}
+	function makeRequest(params, callback) {
+		let ajax = new XMLHttpRequest();
+		ajax.open("POST", endpoint, true);
+		ajax.setRequestHeader(
+			"Content-type",
+			"application/x-www-form-urlencoded"
+		);
+		ajax.onload = callback;
+		ajax.send(params);
+	}
 })();
--- front/js/config.js
+++ front/js/config.js
@@ -1,6 +1,5 @@
-let endpoint = "http://www.mocky.io/v2/5a9e83f43000003400234cac";
+let endpoint = "http://www.mocky.io/v2/5a9f3b072e0000630074cff4";
 let STATUS_OK = 200;
-
+let SUCESSO_STATUS = "sucesso";
+let ERRO_STATUS = "erro";
 let ajax = new XMLHttpRequest();
-ajax.open("POST", endpoint, true);
-ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
